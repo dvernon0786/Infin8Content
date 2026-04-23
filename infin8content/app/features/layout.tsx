546573ca27f7412575c8f4e0f1f0e6fd72e684db
Damien <engagehubonline@gmail.com>
2026-04-23 11:47:14 +1000

    chore: delete old features, solutions, and resources pages

--- infin8content/app/features/layout.tsx
+++
@@ -1,5 +0,0 @@
-import MarketingShell from '@/components/marketing/MarketingShell';
-
-export default function FeaturesLayout({ children }: { children: React.ReactNode }) {
-  return <MarketingShell>{children}</MarketingShell>;
-}
