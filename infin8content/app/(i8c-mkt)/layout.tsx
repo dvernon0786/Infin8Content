56d0c15cd973b6fac5df0deda9a7fce4cc55ef43
Damien <engagehubonline@gmail.com>
2026-04-23 18:27:02 +1000

    fix: remove duplicate layout wrapper to eliminate double header/footer rendering

--- infin8content/app/(i8c-mkt)/layout.tsx
+++ infin8content/app/(i8c-mkt)/layout.tsx
@@ -1,5 +1,3 @@
-import MktLayout from "@/components/MktLayout";
-
 export default function IkcMktLayout({ children }: { children: React.ReactNode }) {
-  return <MktLayout>{children}</MktLayout>;
+  return <>{children}</>;
 }
