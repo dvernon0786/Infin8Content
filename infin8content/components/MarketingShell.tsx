d4943522adc7bb63eb6da6910cbac1508ddf2077
dvernon0786 <engagehubonline@gmail.com>
2026-04-25 10:14:38 +1000

    chore(ui): add MarketingShell wrapper around MktLayout

---
+++ infin8content/components/MarketingShell.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import MktLayout from './MktLayout';
+
+export default function MarketingShell({ children }: { children: React.ReactNode }) {
+  return <MktLayout>{children}</MktLayout>;
+}
